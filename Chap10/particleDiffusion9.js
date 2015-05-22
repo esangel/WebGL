26dd0b8b4cb9e7d85851dbd1f624db589d25cd7d
esangel <angel@cs.unm.edu>
2015-05-22 17:26:51 -0600

    Last one fixed

--- Chap10/particleDiffusion9.js
+++ Chap10/particleDiffusion9.js
@@ -1,3 +1,6 @@
+var t1, t2;
+t1 = new Date();
+
 var canvas;
 var gl;
 
@@ -6,13 +9,13 @@ var flag = true;
 var texSize = 1024;
 var numPoints = 1000;
 var diffuse = 4.0;
-var pointSize = 10.0;
+var pointSize = 5.0;
 
 var released = 1;
 var time = 0;
-var dt = 0.1;
+var dt = 0.01;
 
-var toggle = true;
+var newPoint = [];
 
 var texCoord = [
     vec2(0, 0),
@@ -28,16 +31,13 @@ var vertices = [
     vec2(1.0, 1.0)
 ];
 
-var points = [];
-var new_points = [];
-
-var program1, program2, program3;
+var program1, program2;
 var framebuffer;
 var texture1, texture2;
 
-var buffer1, buffer2, buffer3;
-
-var vPosition1, vPosition2, vPosition3;
+var buffer;
+var vPosition1, vPosition2;
+var vTexCoord;
 var texLoc;
 
 window.onload = function init() {
@@ -45,8 +45,6 @@ window.onload = function init() {
 
     gl = WebGLUtils.setupWebGL( canvas );
     if ( !gl ) { alert( "WebGL isn't available" ); }
-
-    document.getElementById("Button").onclick = function(){toggle = !toggle;};
 
     gl.viewport(0, 0, texSize, texSize);
     gl.activeTexture( gl.TEXTURE0 );
@@ -77,8 +75,6 @@ window.onload = function init() {
 
     program1 = initShaders( gl, "vertex-shader1", "fragment-shader1" );
     program2 = initShaders( gl, "vertex-shader2", "fragment-shader2" );
-    program3 = initShaders( gl, "vertex-shader3", "fragment-shader3" );
-
 
     framebuffer = gl.createFramebuffer();
     gl.bindFramebuffer( gl.FRAMEBUFFER, framebuffer);
@@ -90,53 +86,48 @@ window.onload = function init() {
     var status = gl.checkFramebufferStatus(gl.FRAMEBUFFER);
     if(status != gl.FRAMEBUFFER_COMPLETE) alert('Framebuffer Not Complete');
 
-    for(var i = 0; i<numPoints; i++) {
-         points[i] = vec2(0.5, 0.5);
-         }
 
-    buffer1 = gl.createBuffer();
-    buffer2 = gl.createBuffer();
-    buffer3 = gl.createBuffer();
+    for(var i = 0; i<numPoints/2; i++) {
+         vertices[4+i] = vec2(0.5, 0.5);
+         vertices[4+i+numPoints/2] = vec2(-0.5, -0.5);
+		 }
+
+    buffer = gl.createBuffer();
 
     gl.useProgram(program2);
+
     gl.uniform1f( gl.getUniformLocation(program2, "pointSize"), pointSize );
     gl.uniform4f( gl.getUniformLocation(program2, "color"), 0.0, 0.0, 0.9, 1.0);
 
-    gl.bindBuffer(gl.ARRAY_BUFFER, buffer1);
-    gl.bufferData(gl.ARRAY_BUFFER, 32, gl.STATIC_DRAW);
+    gl.useProgram(program1);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+    gl.bufferData(gl.ARRAY_BUFFER, 64+8*numPoints, gl.STATIC_DRAW);
     gl.bufferSubData(gl.ARRAY_BUFFER, 0, flatten(vertices));
-
-    gl.bindBuffer(gl.ARRAY_BUFFER, buffer2);
-    gl.bufferData(gl.ARRAY_BUFFER, 32, gl.STATIC_DRAW);
-    gl.bufferSubData(gl.ARRAY_BUFFER, 0, flatten(texCoord));
-
-    gl.bindBuffer(gl.ARRAY_BUFFER, buffer3);
-    gl.bufferData(gl.ARRAY_BUFFER, 8*numPoints, gl.STATIC_DRAW);
-    gl.bindBuffer(gl.ARRAY_BUFFER, buffer1);
-
+    gl.bufferSubData(gl.ARRAY_BUFFER, 32+8*numPoints, flatten(texCoord));
 
     // buffers and vertex arrays
 
-    gl.useProgram(program3);
-    vPosition3 = gl.getAttribLocation( program3, "vPosition3" );
-    gl.enableVertexAttribArray( vPosition3 );
-    gl.vertexAttribPointer( vPosition3, 2, gl.FLOAT, false, 0,0 );
-
-    gl.useProgram(program1);
 
     vPosition1 = gl.getAttribLocation( program1, "vPosition1" );
     gl.enableVertexAttribArray( vPosition1 );
     gl.vertexAttribPointer( vPosition1, 2, gl.FLOAT, false, 0,0 );
 
-    gl.bindBuffer(gl.ARRAY_BUFFER, buffer2);
-    var vTexCoord = gl.getAttribLocation( program1, "vTexCoord");
+    vTexCoord = gl.getAttribLocation( program1, "vTexCoord");
     gl.enableVertexAttribArray( vTexCoord );
-    gl.vertexAttribPointer( vPosition2, 2, gl.FLOAT, false, 0,0 );
-    gl.bindBuffer(gl.ARRAY_BUFFER, buffer1);
+    gl.vertexAttribPointer( vTexCoord, 2, gl.FLOAT, false, 0, 32+8*numPoints );
 
     gl.uniform1i( gl.getUniformLocation(program1, "texture"), 0 );
     gl.uniform1f( gl.getUniformLocation(program1, "d"), 1/texSize );
     gl.uniform1f( gl.getUniformLocation(program1, "s"), diffuse );
+
+    gl.useProgram(program2);
+
+    vPosition2 = gl.getAttribLocation( program2, "vPosition2" );
+    gl.enableVertexAttribArray( vPosition2 );
+    gl.vertexAttribPointer( vPosition2, 2, gl.FLOAT, false, 0,0 );
+
+    gl.useProgram(program1);
 
     gl.bindTexture(gl.TEXTURE_2D, texture2);
 
@@ -146,8 +137,6 @@ window.onload = function init() {
 var render = function(){
 
    // render to texture
-
-if(!toggle) {alert("program stopped"); toggle = true};
 
     gl.useProgram(program1);
 
@@ -164,31 +153,33 @@ if(!toggle) {alert("program stopped"); toggle = true};
 
     }
 
-    //var status = gl.checkFramebufferStatus(gl.FRAMEBUFFER);
-    //if(status != gl.FRAMEBUFFER_COMPLETE) alert('Framebuffer Not Complete');
+    var status = gl.checkFramebufferStatus(gl.FRAMEBUFFER);
+    if(status != gl.FRAMEBUFFER_COMPLETE) alert('Framebuffer Not Complete');
 
+    gl.clear( gl.COLOR_BUFFER_BIT );
     gl.drawArrays( gl.TRIANGLE_STRIP, 0, 4 );
 
     gl.useProgram(program2);
-    gl.bindBuffer(gl.ARRAY_BUFFER, buffer3);
+    gl.enableVertexAttribArray( vPosition2 );
     gl.vertexAttribPointer( vPosition2, 2, gl.FLOAT, false, 0, 0);
     gl.uniform4f( gl.getUniformLocation(program2, "color"), 0.9, 0.0, 0.9, 1.0);
-    gl.drawArrays(gl.POINTS, 0, released);
-    //gl.uniform4f( gl.getUniformLocation(program2, "color"), 0.0, 9.0, 0.0, 1.0);
-    //gl.drawArrays(gl.POINTS, numPoints/2, numPoints/2);
-    gl.bindBuffer(gl.ARRAY_BUFFER, buffer1);
+    gl.drawArrays(gl.POINTS, 4, numPoints/2);
+    gl.uniform4f( gl.getUniformLocation(program2, "color"), 0.0, 9.0, 0.0, 1.0);
+    gl.drawArrays(gl.POINTS, 4+numPoints/2, numPoints/2);
 
     gl.useProgram(program1);
-    gl.bindBuffer(gl.ARRAY_BUFFER, buffer2);
-    gl.vertexAttribPointer( texLoc, 2, gl.FLOAT, false, 0, 0);
-    gl.bindBuffer(gl.ARRAY_BUFFER, buffer1);
+    gl.enableVertexAttribArray( vTexCoord );
+    gl.enableVertexAttribArray( vPosition1 );
+    gl.vertexAttribPointer( texLoc, 2, gl.FLOAT, false, 0, 32+8*numPoints);
+    gl.vertexAttribPointer( vPosition1, 2, gl.FLOAT, false, 0, 0);
 
 
 // render to display
 
+    gl.useProgram(program1);
+
     gl.generateMipmap(gl.TEXTURE_2D);
     gl.bindFramebuffer(gl.FRAMEBUFFER, null);
-
 
     if(flag) gl.bindTexture(gl.TEXTURE_2D, texture2);
       else gl.bindTexture(gl.TEXTURE_2D, texture1);
@@ -206,39 +197,51 @@ if(!toggle) {alert("program stopped"); toggle = true};
     gl.useProgram(program2);
 
 // move particles in a random direction
-
-    for(var i=0; i<released; i++) {
-        do {
-            var dx = 0.01*(2.0*Math.random()-1.0);
-            var dy = 0.01*(2.0*Math.random()-1.0);
-            new_points[i] = add(points[i], vec2(dx, dy));
-        } while((new_points[i][0]<0.1)&&(new_points[i][0]>-0.1)&&
-        (new_points[i][1]<0.1)&&(new_points[i][1]> -0.1));
-        points[i] =  vec2(new_points[i]);
-    }
-
-        time+=dt;
-        if(time>released) if(released<numPoints) released++;
-
-
-
-    gl.bindBuffer(gl.ARRAY_BUFFER, buffer3);
-    gl.bufferSubData(gl.ARRAY_BUFFER,  0, flatten(points));
-
-
-    gl.vertexAttribPointer( vPosition3, 2, gl.FLOAT, false, 0,0 );
-
-    gl.useProgram(program3);
-    gl.drawArrays(gl.POINTS, 0, released);
-
-    gl.bindBuffer(gl.ARRAY_BUFFER, buffer2);
-    gl.vertexAttribPointer( vPosition2, 2, gl.FLOAT, false, 0,0 );
+// wrap arounds
 
     gl.useProgram(program2);
+    gl.enableVertexAttribArray( vPosition2 );
+    gl.vertexAttribPointer( vPosition2, 2, gl.FLOAT, false, 0, 0);
+    gl.uniform4f( gl.getUniformLocation(program2, "color"), 0.0, 0.0, 0.0, 1.0);
+    gl.drawArrays(gl.POINTS, 4, numPoints);
+
+    gl.useProgram(program1);
+    gl.enableVertexAttribArray( vPosition1 );
+    gl.enableVertexAttribArray( vTexCoord );
+    gl.vertexAttribPointer( vPosition1, 2, gl.FLOAT, false, 0, 0);
+    gl.vertexAttribPointer( vTexCoord, 2, gl.FLOAT, false, 0, 32+8*numPoints);
+
+    for(var i=0; i<released; i++) {
+      newPoint[0] = vertices[4+i][0] + 0.01*(2.0*Math.random()-1.0);
+      newPoint[1] = vertices[4+i][1] + 0.01*(2.0*Math.random()-1.0);
+      if((newPoint[0]<-0.25)||(newPoint[0]>0.25)||
+            (newPoint[1]<-0.25)||(newPoint[1]> 0.25)) {
+        vertices[4+i][0] = newPoint[0];
+        vertices[4+i][1] = newPoint[1];
+      }
+      newPoint[0] = vertices[4+i+numPoints/2][0] + 0.01*(2.0*Math.random()-1.0);
+      newPoint[1] = vertices[4+i+numPoints/2][1] + 0.01*(2.0*Math.random()-1.0);
+      if((newPoint[0]>0.25)||(newPoint[0]<-0.25)||
+            (newPoint[1]>0.25)||(newPoint[1]< -0.25)) {
+        vertices[4+i+numPoints/2][0] = newPoint[0];
+        vertices[4+i+numPoints/2][1] = newPoint[1];
+      }
+		time += dt;
+		if(time> released)
+		  if(2*released < numPoints) released++;
+
+    }
+    gl.bufferSubData(gl.ARRAY_BUFFER,  0, flatten(vertices));
 
 // swap textures
 
     flag = !flag;
 
-    requestAnimFrame(render);
+    t2 = new Date()
+    var fps = Math.floor(1000/(t2.valueOf()-t1.valueOf())+0.5);
+    t1 = t2;
+//    console.log(fps);
+
+
+     requestAnimFrame(render);
 }

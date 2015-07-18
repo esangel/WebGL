da8356540d111f4b6bdcc5cf02e869df4f40df8c
Rogério Brito <rbrito@ime.usp.br>
2015-07-18 18:31:35 -0300

    Common/MV: Make indentation easier on the eyes.
    We may want to use an automatic indenter in the near future to take care of
    some other inconsistencies in the code.
    
    Signed-off-by: Rogério Brito <rbrito@ime.usp.br>

--- Common/MV.js
+++ Common/MV.js
@@ -932,10 +932,13 @@ function normalMatrix(m, flag)
 {
     var a = mat4();
     a = inverse(transpose(m));
-    if(flag != true) return a;
+    if (flag != true)
+	return a;
 
     var b = mat3();
-    for(var i=0;i<3;i++) for(var j=0; j<3; j++) b[i][j] = a[i][j];
+    for (var i=0;i<3;i++)
+	for(var j=0; j<3; j++)
+	    b[i][j] = a[i][j];
     return b;
 
 }

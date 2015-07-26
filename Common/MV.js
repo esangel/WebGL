19c2e19d15acfdbc2615dbd5f32b3840d2f00c89
esangel <angel@cs.unm.edu>
2015-07-26 11:03:59 -0600

    Added rotateX, rotateY, rotateZ to MV.js

--- Common/MV.js
+++ Common/MV.js
@@ -364,6 +364,33 @@ function rotate( angle, axis )
 
     return result;
 }
+function rotateX(theta) {
+  var c = Math.cos( radians(theta) );
+  var s = Math.sin( radians(theta) );
+  var rx = mat4( 1.0,  0.0,  0.0, 0.0,
+      0.0,  c,  s, 0.0,
+      0.0, -s,  c, 0.0,
+      0.0,  0.0,  0.0, 1.0 );
+  return rx;
+}
+function rotateY(theta) {
+  var c = Math.cos( radians(theta) );
+  var s = Math.sin( radians(theta) );
+  var ry = mat4( c, 0.0, -s, 0.0,
+      0.0, 1.0,  0.0, 0.0,
+      s, 0.0,  c, 0.0,
+      0.0, 0.0,  0.0, 1.0 );
+  return ry;
+}
+function rotateZ(theta) {
+  var c = Math.cos( radians(theta) );
+  var s = Math.sin( radians(theta) );
+  var rz = mat4( c, -s, 0.0, 0.0,
+      s,  c, 0.0, 0.0,
+      0.0,  0.0, 1.0, 0.0,
+      0.0,  0.0, 0.0, 1.0 );
+  return rz;
+}
 
 //----------------------------------------------------------------------------
 
